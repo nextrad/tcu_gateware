aca911b1fb73c2bca2aeb15e6e908e569917c7bf
bradkahn <bradkahn92@gmail.com>
2018-05-15 07:14:18 +0200

    diff clk

--- hdl/simple_pri.vhd
+++ hdl/simple_pri.vhd
@@ -9,8 +9,11 @@ use unisim.vcomponents.all;
 
 entity simple_pri is
     port (
-        clk_IN  : in  std_logic;
-        pri_OUT : out std_logic
+--        clk_IN  : in  std_logic;
+        clk_P_IN  : in  std_logic;
+        clk_N_IN  : in  std_logic;
+        pri_OUT : out std_logic;
+		  logic_high : out std_logic  -- supply power to J1 P40 to feed ext clock converter
     );
 end entity;
 
@@ -20,16 +23,30 @@ signal clk_sig : std_logic := '0';
 signal counter : unsigned(31 downto 0) := (others => '0');
 
 begin
+	logic_high <= '1'; -- supply power to J1 P40 to feed ext clock converter
+--    IBUFG_inst : IBUFG
+--    generic map (
+--        IBUF_LOW_PWR => FALSE,
+--        IOSTANDARD => "DEFAULT"
+--    )
+--    port map (
+--        O => clk_sig,
+--        I => clk_IN
+--    );
 
-    IBUFG_inst : IBUFG
-    generic map (
-        IBUF_LOW_PWR => FALSE,
-        IOSTANDARD => "DEFAULT"
-    )
-    port map (
-        O => clk_sig,
-        I => clk_IN
-    );
+	 IBUFGDS_sys_clk: IBUFGDS
+	generic map
+	(
+		IOSTANDARD => "LVDS_25",
+		DIFF_TERM => TRUE,
+		IBUF_LOW_PWR => FALSE
+	)
+	port map
+	(
+		I => clk_P_IN,
+		IB => clk_N_IN,
+		O => clk_sig
+	);
 
     process(clk_sig)
     begin
@@ -38,7 +55,7 @@ begin
         end if;
     end process;
 
-    -- pri_OUT <= counter(15); -- 1525.9Hz (100MHz clk)
-    pri_OUT <= counter(12); -- 1220.7Hz (10MHz clk)
+	 pri_OUT <= counter(15); -- 1525.9Hz (100MHz clk)
+    --pri_OUT <= counter(12); -- 1220.7Hz (10MHz clk)
 
 end architecture;
